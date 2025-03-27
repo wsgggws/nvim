wsgggws/nvim-plugs

fix: update colorschema

lua/plugins/colorschema.lua
@@ -41,11 +41,11 @@ return {
 			-- local colorscheme = "catppuccin-mocha",
 			-- local colorscheme = "gruvbox-material",
 			if os.getenv("TERM_PROGRAM") == "WezTerm" then
-				-- colorscheme = "solarized"
+				colorscheme = "solarized"
 				-- colorscheme = "dracula"
 				-- colorscheme = "gruvbox"
 				-- colorscheme = "tokyonight-night"
-				colorscheme = "catppuccin"
+				-- colorscheme = "catppuccin"
 			end
 			return {
 				colorscheme = colorscheme,

wsgggws/nvim

lua/plugins/colorschema.lua
@@ -42,10 +42,10 @@ return {
 			-- local colorscheme = "gruvbox-material",
 			if os.getenv("TERM_PROGRAM") == "WezTerm" then
 				-- colorscheme = "solarized"
-				-- colorscheme = "dracula"
+				colorscheme = "dracula"
 				-- colorscheme = "gruvbox"
 				-- colorscheme = "tokyonight-night"
-				colorscheme = "catppuccin"
+				-- colorscheme = "catppuccin"
 			end
 			return {
 				colorscheme = colorscheme,