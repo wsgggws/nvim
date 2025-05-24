d0e167d327a200561958036ab5881228dcd4b282
wsgggws <wsgggws@gmail.com>
2025-05-24 11:07:53 +0800

    fix: conform ruff configuration

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,9 @@ return {
 	optional = true,
 	opts = {
 		formatters_by_ft = {
-			["python"] = { "ruff_format", "isort" },
+			-- 这种配置 看官方文档 *绝对* 比问 GPT 可靠
+			-- https://docs.astral.sh/ruff/editors/setup/#vim  (With the conform.nvim plugin for Neovim.)
+			["python"] = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 		},
 	},
 }
