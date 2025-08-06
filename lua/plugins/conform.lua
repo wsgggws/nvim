8b0da83676799f82911698c740613112705a8ae8
wsgggws <wsgggws@gmail.com>
2025-08-06 15:13:51 +0800

    fix: conform opt auto save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,10 +2,6 @@ return {
 	"stevearc/conform.nvim",
 	optional = true,
 	opts = {
-		format_on_save = {
-			timeout_ms = 3000,
-			lsp_fallback = true,
-		},
 		formatters_by_ft = {
 			-- 这种配置 看官方文档 *绝对* 比问 GPT 可靠
 			-- https://docs.astral.sh/ruff/editors/setup/#vim  (With the conform.nvim plugin for Neovim.)
