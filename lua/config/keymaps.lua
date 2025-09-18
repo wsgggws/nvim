d01b7bb5f0f68d1192e7e68a7d1ddeafd0c05f2e
wsgggws <wsgggws@gmail.com>
2025-09-18 12:29:55 +0800

    feat: add fmt skip shortkey

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,27 @@ vim.api.nvim_set_keymap("n", "<c-s-j>", "<cmd>res +2<CR>", { noremap = true, sil
 vim.api.nvim_set_keymap("n", "<c-s-k>", "<cmd>res -2<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<c-s-h>", "<cmd>vertical resize -2<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<c-s-l>", "<cmd>vertical resize +2<CR>", { noremap = true, silent = true })
+
+-- 定义 toggle 函数
+_G.toggle_fmt_skip = function(start_line, end_line)
+	local buf = 0
+	for i = start_line, end_line do
+		local line = vim.api.nvim_buf_get_lines(buf, i - 1, i, false)[1]
+		if line:match("# fmt: skip%s*$") then
+			-- 删除尾部 fmt: skip
+			local new_line = line:gsub("%s*# fmt: skip%s*$", "")
+			vim.api.nvim_buf_set_lines(buf, i - 1, i, false, { new_line })
+		else
+			-- 添加尾部 fmt: skip
+			vim.api.nvim_buf_set_lines(buf, i - 1, i, false, { line .. " # fmt: skip" })
+		end
+	end
+end
+
+-- 普通模式 toggle 当前行
+vim.api.nvim_set_keymap(
+	"n",
+	"gcs",
+	[[:lua _G.toggle_fmt_skip(vim.fn.line("."), vim.fn.line("."))<CR>]],
+	{ noremap = true, silent = true }
+)
