104fa5073b66adf83e64ba427e6f45b1c0aec51f
wsgggws <wsgggws@gmail.com>
2026-01-11 00:02:35 +0800

    feat: use myself particles in sparks.nvim

--- lua/plugins/sparks.lua
+++ lua/plugins/sparks.lua
@@ -4,5 +4,27 @@ return {
 	event = "VeryLazy",
 	opts = {
 		-- 🚀 默认已启用所有最佳配置
+		triggers = {
+			-- 自定义特效绑定示例
+			["@"] = "nghuhu",
+			["/"] = "nghuhu",
+			["&"] = "nghuhu",
+			["+"] = "nghuhu",
+			["?"] = "shenyiao",
+			["!"] = "shenyiao",
+			["`"] = "shenyiao",
+			["-"] = "shenyiao",
+			["{"] = "yueyue",
+			["("] = "yueyue",
+			["["] = "yueyue",
+			["<"] = "yueyue",
+			["="] = "yueyue",
+			[";"] = "manman",
+			[","] = "manman",
+			["."] = "manman",
+			["'"] = "manman",
+			['"'] = "manman",
+			[":"] = "manman",
+		},
 	},
 }
