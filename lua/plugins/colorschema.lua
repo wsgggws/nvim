3a64fb7b5a9429ff561085d8e6007dcb8eb4c2ed
wsgggws <wsgggws@gmail.com>
2026-06-08 13:25:04 +0800

    feat: update theme to dracula

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -8,55 +8,14 @@ return {
 			-- transparent_background = true,
 		},
 	},
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	opts = {
-	-- 		style = "day",
-	-- 	},
-	-- },
-	-- {
-	-- 	"ellisonleao/gruvbox.nvim",
-	-- 	opts = {
-	-- 		transparent_mode = true,
-	-- 		contrast = "soft",
-	-- 		invert_tabline = true,
-	-- 		invert_selection = true,
-	-- 		invert_intend_guides = true,
-	-- 		overrides = {
-	-- 			Normal = { fg = nil, bg = "#ebdbb2" },
-	-- 		},
-	-- 	},
-	-- },
-	-- {
-	-- 	"dracula/vim",
-	-- 	name = "dracula",
-	-- },
-	-- {
-	-- 	"Mofiqul/dracula.nvim",
-	-- 	name = "dracula",
-	-- 	opts = {
-	-- 		transparent_bg = true,
-	-- 	},
-	-- },
-	-- {
-	-- 	"p00f/alabaster.nvim",
-	-- 	name = "alabaster",
-	-- },
-	-- {
-	-- 	"maxmx03/solarized.nvim",
-	-- },
+	{
+		"dracula/vim",
+		name = "dracula",
+	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			-- colorscheme = "gruvbox",
-			-- colorscheme = "tokyonight-day",
-			colorscheme = "catppuccin-mocha",
-			-- colorscheme = "gruvbox-material",
-			-- colorscheme = "solarized"
-			-- colorscheme = "dracula",
-			-- colorscheme = "alabaster",
-			-- colorscheme = "tokyonight-night",
-			-- colorscheme = "tokyonight",
+			colorscheme = "dracula",
 			-- colorscheme = "catppuccin",
 		},
 	},
